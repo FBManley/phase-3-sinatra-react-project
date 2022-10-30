c9d6f450ea7a27f141f3a9cd3c8334cd592ea7d1
Jacob Manley <jbmanley45@gmail.com>
2022-10-30 16:23:05 -0700

    ORM

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,39 @@ class User < ActiveRecord::Base
 # defines data structure -> updates View
 #  interacts w the database
     has_many :tasks
-    # attr_accessor :name
+    attr_accessor :name, age:, breed:, :id
     
-    def initialize(name)
+    def initialize(name:, age:, breed:, id: nil)
         @name = name
+        @id = id 
+        @age = age
+        @breed = breed
     end
+
+    def self.create_table
+        sql =  <<-SQL
+          CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            age INTEGER, 
+            breed TEXT
+          )
+          SQL
+        DB[:conn].execute(sql)
+    end
+
+    def save
+        sql = <<-SQL
+          INSERT INTO users (name, age, breed)
+          VALUES (?, ?)
+        SQL
+    
+        DB[:conn].execute(sql, self.name, self.album)
+    
+      end
+
+
+
 # The new method could make available to the view a @user instance variable by creating a new User:
     def new 
         @user.new 
@@ -20,3 +48,5 @@ class User < ActiveRecord::Base
     # def self.create_table
     # end
 end
+# NOTES:
+# When building an ORM to connect our Ruby program to a database, we equate a class with a database table and the instances that the class produces to rows in that table.
--- config/environment.rb
+++ config/environment.rb
@@ -8,3 +8,6 @@ Bundler.require(:default, ENV['RACK_ENV'])
 
 # Require in all files in 'app' directory
 require_all 'app'
+
+# DB, that is equal to a hash that contains our connection to the database. Gives us access tp the DB constant and the database connection
+DB = { conn: SQLite3::Database.new("db/user_database.db")}
