c536c3ca175510ea65ee35de9029a8824af046fb
Jacob Manley <jbmanley45@gmail.com>
2022-11-06 22:26:58 -0800

    backend 85%

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,5 +29,24 @@ RecordLabel.create(name: "soft n swift llc", record_labels_id: 3)
 # User.create(name: "Jake", age: "99")
 # User.create(name: "Bob", age: "23")
 # User.create(name: "Rachel", age: "54")
+#50.times do
+  # create a game with random data
+#   game = Game.create(
+#     title: Faker::Game.title,
+#     genre: Faker::Game.genre,
+#     platform: Faker::Game.platform,
+#     price: rand(0..60) # random number between 0 and 60
+#   )
+
+#   # create between 1 and 5 reviews for each game
+#   rand(1..5).times do
+#     Review.create(
+#       score: rand(1..10),
+#       comment: Faker::Lorem.sentence,
+#       game_id: game.id # use the ID (primary key) of the game as the foreign key
+#     )
+#   end
+
+end
 
 puts "✅ Done seeding!"
