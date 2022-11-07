ef0778ddd0b6360e6304c197ff8a09d2692c38d9
Jacob Manley <jbmanley45@gmail.com>
2022-11-06 22:11:45 -0800

    cleaned up models

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,9 +1,3 @@
 class Venue < ActiveRecord::Base
-    # has_many :record_labels
-    # create / read only
     belongs_to :artist
-    # has_many :artists
-    # venue table needs artist_id
-    # build_artists
-    # @artist = @venue.create_artist()
 end
