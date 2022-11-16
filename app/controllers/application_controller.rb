84e9e045055094a4d3510eb0b23ebd4cdb15b0c9
Jake Manley <jbmanley45@gmail.com>
2022-11-16 09:28:19 -0800

    port issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < Sinatra::Base
   # connects the Model. Sends response to the View.Interface between React and our database is our API
   # full CRUD
   get "/artists" do 
-    artists = Artist.all.order(:name) 
-    artists.to_json(include: [:record_labels]) 
+    artists = Artist.all
+    artists.to_json() 
   end
 
   get "/record_labels" do 
@@ -13,27 +13,32 @@ class ApplicationController < Sinatra::Base
     rl.to_json(include: [:artists])
   end
 
-  get "/artists/:id" do 
-    artist = Artist.find_by_id(params["id"])
-    artist.to_json(include: [:venues])
-  end
+  # get "/record_labels/:id" do
+  #   rl = RecordLabel.find(params[:id])
+  #   rl.to_json()
+  # end
 
-  post "/artists" do 
-    artist = Artist.create(name: params[:name], albums: params[:albums])
-    artist.to_json
-  end
+  # get "/artists/:id" do 
+  #   artist = Artist.find_by_id(params[:id])
+  #   artist.to_json()
+  # end
 
-  patch "/artists/:id" do 
-    artists = Artist.find(params[:id])
-    artists.update(name: params[:name], albums: params[:albums])
-    artists.to_json
-  end
+  # post "/artists" do 
+  #   artist = Artist.create(name: params[:name], albums: params[:albums])
+  #   artist.to_json
+  # end
 
-  delete "/artists/:id" do
-    artist = Artist.find(params[:id])
-    artist.destroy 
-    artist.to_json
-  end
+  # patch "/artists/:id" do 
+  #   artists = Artist.find(params[:id])
+  #   artists.update(name: params[:name], albums: params[:albums])
+  #   artists.to_json
+  # end
+
+  # delete "/artists/:id" do
+  #   artist = Artist.find(params[:id])
+  #   artist.destroy 
+  #   artist.to_json
+  # end
 
   # Read and Create
   # get "/record_labels" do 
